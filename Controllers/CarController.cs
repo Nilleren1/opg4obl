2fa09112018ecba98653b9208365a974198b60ae
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-03-07 18:07:30 +0100

    Add project files.

---
+++ Controllers/CarController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using opg1obl;
+using opg4obl.Manager;
+
+namespace opg4obl.Controllers
+{
+
+    
+        [Route("api/[controller]")]
+        [ApiController]
+        public class CarController : ControllerBase
+        {
+            private CarManager _carManager = new CarManager();
+            // GET: api/<ItemsController>
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
+            [HttpGet]
+            public ActionResult<IEnumerable<Car>> Get([FromQuery] string? filterstring,  /*Her laver jeg en header som parameter:  */ [FromHeader] int itemAmount)
+            {
+                IEnumerable<Car> items = _carManager.GetAll(filterstring);
+                //Her laver vi vores egen header.
+                //string testHeader = Request.Headers["TestHeader"];
+
+                Response.Headers.Add("Total-Amount", items.Count().ToString());
+
+
+                if (items == null || items.Count() == 0)
+                {
+                    return NotFound("Den kunne den altså ikke finde");
+                }
+                if (itemAmount > 0)
+                {
+                    return Ok(items.Take(itemAmount));
+                }
+                else
+                {
+                    return Ok(items);
+                }
+            }
+
+
+            //GET: api/items/Name/<value>
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
+            [Route("Name/{filterstring}")]
+            [HttpGet]
+            public ActionResult<IEnumerable<Car>> GetFilterstring(string filterstring)
+            {
+                IEnumerable<Car> items = _carManager.GetAll(filterstring);
+                if (items == null || items.Count() == 0)
+                {
+                    return NotFound("Den kunne den altså ikke finde");
+                }
+
+                return Ok(items);
+            }
+
+            //api/items/Quality/<value>
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
+            [Route("Price/{Price}")]
+            [HttpGet]
+            public ActionResult<IEnumerable<Car>> GetByPrice(double Price)
+            {
+                IEnumerable<Car> items = _carManager.GetByPrice(Price);
+                if (items == null)
+                {
+                    return NotFound("Price could not be found");
+                }
+                return Ok(items);
+            }
+
+
+            // GET api/<ItemsController>/5
+
+            [HttpGet("{id}")]
+            public Car GetId(int id)
+            {
+                return _carManager.GetById(id);
+            }
+
+            // POST api/<ItemsController>[ProducesResponseType(StatusCodes.Status200OK)]
+            [ProducesResponseType(StatusCodes.Status404NotFound)]
+            [ProducesResponseType(StatusCodes.Status200OK)]
+            [HttpPost]
+            public Car Post([FromBody] Car value)
+            {
+                return _carManager.AddCar(value);
+            }
+
+            // PUT api/<ItemsController>/5
+            [HttpPut("{id}")]
+            public Car Put(int id, [FromBody] Car value)
+            {
+                return _carManager.UpdateCar(id, value);
+            }
+
+        // DELETE api/<ItemsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id}")]
+            public OkObjectResult Delete(int id)
+            {
+                return Ok(_carManager.DeleteCar(id));
+            }
+        }
+    }
+
---
+++ Manager/CarManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using opg1obl;
+
+namespace opg4obl.Manager
+{
+    public class CarManager
+    {
+        private static int nextID = 1;
+        private static List<Car> data = new List<Car>()
+        {
+            new Car() {Id = nextID++, LicensePlate = "af92u95", Model = "Axios", Price = 24050},
+            new Car() {Id = nextID++, LicensePlate = "az62u95", Model = "Susuki", Price = 29000},
+            new Car() {Id = nextID++, LicensePlate = "kf82091", Model = "Golf", Price = 124000},
+            new Car() {Id = nextID++, LicensePlate = "yj38805", Model = "Audi", Price = 124},
+            new Car() {Id = nextID++, LicensePlate = "hg93019", Model = "Ferrari", Price = 24300}
+           
+        };
+
+        /// <summary>
+        /// A getAll method which you can filter in the url. 
+        /// </summary>
+        /// <param name="filterstring"></param>
+        /// <returns>result</returns>
+        public List<Car> GetAll(string filterstring)
+        {
+            List<Car> result = new List<Car>(data);
+            if (!string.IsNullOrWhiteSpace(filterstring))
+            {
+                result = data.FindAll(c => c.Model.Contains(filterstring, StringComparison.OrdinalIgnoreCase));
+            }
+            return result;
+        }
+
+
+        public List<Car> GetByPrice(double carPrice)
+        {
+            List<Car> result;
+            if (carPrice <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                result = data.FindAll(car => car.Price <= carPrice);
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Finder en enkelt car, med det pågældende ID.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns>En car med det korrekte ID</returns>
+        public Car GetById(int ID)
+        {
+            //lærens løsning.
+            return data.Find(car => car.Id == ID);
+
+
+
+            //foreach (Item item in data)
+            //{
+            //    if (item.ID = ID)
+            //    {
+            //        return item;
+            //    }
+
+            //    return null;
+            //}
+        }
+
+        public Car AddCar(Car car)
+        {
+            car.Id = nextID++;
+            data.Add(car);
+
+            return car;
+        }
+
+        public Car DeleteCar(int id)
+        {
+            Car car = GetById(id);
+
+            if (car.Id == id)
+            {
+                data.Remove(car);
+            }
+
+            return car;
+
+        }
+
+        public Car UpdateCar(int id, Car carToBeUpdated)
+        {
+            var car = GetById(id);
+            if (car == null)
+            {
+                return null;
+            }
+            else
+            {
+                car.Model = carToBeUpdated.Model;
+                car.LicensePlate = carToBeUpdated.LicensePlate;
+                car.Price = carToBeUpdated.Price;
+            }
+            return carToBeUpdated;
+        }
+    }
+}
